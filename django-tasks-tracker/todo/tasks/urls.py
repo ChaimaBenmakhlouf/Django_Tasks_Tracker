a9812a6f78daf276db95be8f8b523f0e057620f1
ChaimaBenmakhlouf <chaima.benmakhlouf@supinternet.fr>
2021-11-04 17:30:21 +0100

    remove an url from file

--- django-tasks-tracker/todo/tasks/urls.py
+++ django-tasks-tracker/todo/tasks/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('home/', views.home, name='home'),
-    path('', views.home, name='register'),
     path('register/', views.register, name='register'),
     path('', views.index, name="list"),
     path('update_task/<str:pk>/', views.updateTask, name="update_task"),
